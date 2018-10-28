27e2f8594cb7f8f8f454b217b8521848d19dddba
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-10-28 17:16:54 +0300

    feat: - *add test of sumOfOther*

--- test.js
+++ test.js
@@ -5,3 +5,30 @@ Object.freeze(assert);
 const sumOfOther = require('./src/sumOfOther.js');
 const make = require('./src/make.js');
 const recursion = require('./src/recursion.js');
+
+describe('sumOfOther', () => {
+  it('1', () => {
+    const result = sumOfOther([2, 3, 4, 1]);
+    assert.deepEqual(result, [8, 7, 6, 9]);
+  });
+
+  it('2', () => {
+    const result = sumOfOther([1, 0, 2, 1]);
+    assert.deepEqual(result, [3, 4, 2, 3]);
+  });
+
+  it('3', () => {
+    const result = sumOfOther([2, 8, 4, 1]);
+    assert.deepEqual(result, [13, 7, 11, 14]);
+  });
+
+  it('4', () => {
+    const result = sumOfOther([2, 4, 4, 3]);
+    assert.deepEqual(result, [11, 9, 9, 10]);
+  });
+
+  it('5', () => {
+    const result = sumOfOther([2, 1, 5, 1]);
+    assert.deepEqual(result, [7, 8, 4, 8]);
+  });
+});
