33738ebc2e18b1bdfb877995501cdab385afc067
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-10-28 17:20:39 +0300

    feat: - *add test of recursion function*

--- test.js
+++ test.js
@@ -64,3 +64,35 @@ describe('make', () => {
     assert.equal(result, 8);
   });
 });
+
+describe('recursion', () => {
+  it('1', () => {
+    const tree = { value: 100, left: { value: 90, left: { value: 70 }, right: { value: 99 } }, right: { value: 120, left: { value: 110 }, right: { value: 130 } } };
+    const result = recursion(tree);
+    assert.deepEqual(result, [[100], [90, 120], [70, 99, 110, 130]]);
+  });
+
+  it('2', () => {
+    const tree = { value: 110, left: { value: 100, left: { value: 80 }, right: { value: 109 } }, right: { value: 130, left: { value: 120 }, right: { value: 140 } } };
+    const result = recursion(tree);
+    assert.deepEqual(result, [[110], [100, 130], [80, 109, 120, 140]]);
+  });
+
+  it('3', () => {
+    const tree = { value: 100, left: { value: 90, left: { value: 70 } }, right: { value: 120, right: { value: 130 } } };
+    const result = recursion(tree);
+    assert.deepEqual(result, [[100], [90, 120], [70, 130]]);
+  });
+
+  it('4', () => {
+    const tree = { value: 100, left: { value: 90 }, right: { value: 120 } };
+    const result = recursion(tree);
+    assert.deepEqual(result, [[100], [90, 120]]);
+  });
+
+  it('5', () => {
+    const tree = { value: 100, left: { value: 90, left: { value: 70, left: { value: 65 } }, right: { value: 99 } }, right: { value: 120, left: { value: 110 }, right: { value: 130, right: { value: 200 } } } };
+    const result = recursion(tree);
+    assert.deepEqual(result, [[100], [90, 120], [70, 99, 110, 130], [65, 200]]);
+  });
+});
